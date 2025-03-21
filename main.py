9967a7689c58518c9b9564bddcf078e9fef6dca6
Rebecca Benton <becky@beckybenton.com>
2025-03-21 09:11:01 -0700

    testing redeployement

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def favicon():
 @app.post('/hello', response_class=HTMLResponse)
 async def hello(request: Request, name: str = Form(...)):
     if name:
-        print('Request for hello page received with name=%s' % name)
+        print(f'Request for hello page received with name={name}')
         return templates.TemplateResponse('hello.html', {"request": request, 'name':name})
     else:
         print('Request for hello page received with no name or blank name -- redirecting')
